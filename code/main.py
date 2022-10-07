23f311492cb320ada6586c669e9dee7342136cc4
Pranjal <pranjalmaheshka@gmail.com>
2022-10-06 22:59:07 -0500

    Scaffold v1

--- code/main.py
+++ code/main.py
@@ -1,1 +1,24 @@
 ## Main file
+
+def get_urls(infile)
+    return list_urls
+
+def get_profs(url)
+    return profs
+
+def get_prof_info(prof)
+    return prof_info
+
+def get_h_index(prof_info)
+    return prof_h_index
+
+def tabulate_data(prof_info)
+    return None
+
+def run_main()
+    return None
+
+if __name__ == "__main__":
+    run)main()
+    # code for testing
+    
