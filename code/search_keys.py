bb37f475736bbffce930ab041062422cdd842f4b
soojeechoi <soojeechoi2@gmail.com>
2022-10-12 15:18:52 -0500

    search_keys

--- code/search_keys.py
+++ code/search_keys.py
@@ -1,18 +1,18 @@
 def search_keys():
 	school_search = dict()
 
-	school_search.update({"Harvard University": ["Harvard University", "Harvard"]})
-	school_search.update({"Stanford University": ["Stanford University", "Stanford"]})
-	school_search.update({"Princeton University": ["Princeton University", "Princeton"]})
+	school_search.update({"Harvard": ["Harvard University", "Harvard"]})
+	school_search.update({"Stanford": ["Stanford University", "Stanford"]})
+	school_search.update({"Princeton": ["Princeton University", "Princeton"]})
 	school_search.update({"University of Chicago": ["University of Chicago"]})
-	school_search.update({"Yale University": ["Yale"]})
-	school_search.update({"Northwestern University": ["Northwestern University", "Northwestern"]})
-	school_search.update({"Columbia University": ["Columbia University", "Columbia"]})
+	school_search.update({"Yale": ["Yale"]})
+	school_search.update({"Northwestern": ["Northwestern University", "Northwestern"]})
+	school_search.update({"Columbia": ["Columbia University", "Columbia"]})
 	school_search.update({"New York University": ["New York University"]})
 	school_search.update({"University of Wisconsin-Madison": ["University of Wisconsin-Madison", "University of Wisconsin - Madison", "University of Wisconsin", "University of wisconsin"]})
-	school_search.update({"Duke University": ["Duke University", "Duke"]})
-	school_search.update({"Brown University": ["Brown University", "Brown"]})
+	school_search.update({"Duke": ["Duke University", "Duke"]})
+	school_search.update({"Brown": ["Brown University", "Brown"]})
 	school_search.update({"Boston University": ["Boston University", "Boston"]})
-	school_search.update({"Univeristy of Texas at Austin": ["UT Austin","University of Texas at Austin", "University of Texas", "The University of Texas at Austin"]})
+	school_search.update({"UT Austin": ["UT Austin","University of Texas at Austin", "University of Texas", "The University of Texas at Austin"]})
 
 	return school_search
