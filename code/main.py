6758bb862a9226edcf3da004be4c055a2a275145
Annie Nguyen <annienguyen879@gmail.com>
2022-10-11 23:50:07 -0500

    finished cleaning professors titles

--- code/main.py
+++ code/main.py
@@ -1,10 +1,7 @@
 ## Main file
 import os
 import pandas as pd
-
-
 from common import get_soup
-
 from scrape_pages import scrape_all
 
 colleges = [
@@ -14,21 +11,40 @@ colleges = [
     'hopkins','bu','wisconsin','uta'
     ]
 
+def clean_titles():
+    data = scrape_all()
+
+    output = []
+    for prof in data:
+        title = prof[2].lower()
+        if "research assistant professor" in title:
+            continue
+        elif "associate professor" in title:
+            output.append([prof[0],prof[1],"Associate Professor"])
+        elif "assistant professor" in title:
+            output.append([prof[0],prof[1],"Assistant Professor"])
+        elif "professor" in title:
+            output.append([prof[0],prof[1],"Professor"])
+        else:
+            continue
+
+    return output
+
 
 def get_data():
     '''Getting a final data as lists of lists and converting it to a Pandas df.'''
-    
+
     raw_data = scrape_all()
-    
+
     # send raw data to h-index file and add info
 
-    raw_df = pd.DataFrame(raw_data, columns = ['University', 'Professor', "Title"])
-    
+    raw_df = pd.DataFrame(raw_data, columns = ['University', 'Professor', 'Title'"'])
+
     # final df needs to have relevant title + Google Scholar info
 
-    #print(raw_df)    
-
-get_data()
+    #print(raw_df)
+    return None
+#get_data()
 
 def get_h_index(prof_info):
 
@@ -46,4 +62,3 @@ if __name__ == "__main__":
     run)main()
     # code for testing
 '''
-
