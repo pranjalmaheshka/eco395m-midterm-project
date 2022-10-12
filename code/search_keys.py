20aa30885619e5d7ce519400ce3a0cb9ae4fa88f
soojeechoi <soojeechoi2@gmail.com>
2022-10-12 14:57:49 -0500

    search_keys

---
+++ code/search_keys.py
@@ -0,0 +1,18 @@
+def search_keys():
+	school_search = dict()
+
+	school_search.update({"Harvard University": ["Harvard University", "Harvard"]})
+	school_search.update({"Stanford University": ["Stanford University", "Stanford"]})
+	school_search.update({"Princeton University": ["Princeton University", "Princeton"]})
+	school_search.update({"University of Chicago": ["University of Chicago"]})
+	school_search.update({"Yale University": ["Yale"]})
+	school_search.update({"Northwestern University": ["Northwestern University", "Northwestern"]})
+	school_search.update({"Columbia University": ["Columbia University", "Columbia"]})
+	school_search.update({"New York University": ["New York University"]})
+	school_search.update({"University of Wisconsin-Madison": ["University of Wisconsin-Madison", "University of Wisconsin - Madison", "University of Wisconsin", "University of wisconsin"]})
+	school_search.update({"Duke University": ["Duke University", "Duke"]})
+	school_search.update({"Brown University": ["Brown University", "Brown"]})
+	school_search.update({"Boston University": ["Boston University", "Boston"]})
+	school_search.update({"Univeristy of Texas at Austin": ["UT Austin","University of Texas at Austin", "University of Texas", "The University of Texas at Austin"]})
+
+	return school_search
