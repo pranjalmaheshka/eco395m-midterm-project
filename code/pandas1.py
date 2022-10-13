7fa8e80721d97de72f11c9a333c0a0d9b776461f
Annie Nguyen <annienguyen879@gmail.com>
2022-10-12 20:28:32 -0500

    added tables

--- code/pandas1.py
+++ code/pandas1.py
@@ -1,16 +1,41 @@
 import pandas as pd
+from main import get_h_index
 
-data = [['Boston University', 'Kevin Lang','Professor', '12402','4363','52','29'],\
-['Harvard','Emily Breza','Professor','1867','1690','21','21'],\
-['Princeton','Xiaosheng Mu','Assistant Professor','203','197','9','9']]
+# data = get_h_index()
+
+data = [
+    ["Boston University", "Kevin Lang", "Professor", "12402", "4363", "52", "29"],
+    ["Harvard", "Emily Breza", "Professor", "1867", "1690", "21", "21"],
+    ["Princeton", "Xiaosheng Mu", "Assistant Professor", "203", "197", "9", "9"],
+]
 
 
-df = pd.DataFrame(data, columns = ["university","name","title","h_index","h_index2017","citations","citations2017"])
-df[["h_index","h_index2017","citations","citations2017"]] = df[["h_index","h_index2017","citations","citations2017"]].apply(pd.to_numeric)
+df = pd.DataFrame(
+    data,
+    columns=[
+        "university",
+        "name",
+        "title",
+        "h_index",
+        "h_index2017",
+        "citations",
+        "citations2017",
+    ],
+)
+df[["h_index", "h_index2017", "citations", "citations2017"]] = df[
+    ["h_index", "h_index2017", "citations", "citations2017"]
+].apply(pd.to_numeric)
 
 
-#grouping by the different titles, we take the average of the values
-by_title = df.groupby("title").mean()
+# grouping by the different titles, we take the average of the values
+# print(df.groupby("title").mean())
 
-print(by_title)
-print(by_title.plot.bar())
+print("For each uni, this is the number of profs who have each title.")
+print(df.groupby(["university", "title"]).size())
+
+print("Average value for each index by University and Job Title")
+print(
+    df.groupby(["university", "title"])
+    .mean(numeric_only=True)
+    .sort_values(by=["h_index"], ascending=False)
+)
